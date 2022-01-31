87c9d29f4e01139957ae9efd481baf3746e1e13c
Chinmay Kulkarni <64392365+CRK9587@users.noreply.github.com>
2022-02-01 01:48:51 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('covi11.mysql.database.azure.com','covi11@covi11','Chinu@2018','covihospital_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('covi11.mysql.database.azure.com','covi11@covi11','crk@2022','covihospital_db')or die("Could not connect to mysql".mysqli_error($con));
